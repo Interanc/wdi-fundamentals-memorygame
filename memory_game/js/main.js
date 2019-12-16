394ea60dcf3f7dcdb4678037f0c743841c8917bb
Jamal Maudib McDowell <jamalmaudibmcdowell@192.168.1.4>
2019-12-16 01:24:18 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-var card1 = king;
-var card2 = queen;
-var card3 = king;
-var card4 = queen;
 
-console.log("User Flipped " + card1);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[3];
+var cardsInPlay = [];
+
+
+
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (length.cardsInPlay === 2){
+}
+  else if  (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You Found A Match!")
+  }
+
+  else {
+    alert("Sorry, Try again")
+  }
+
+
+console.log("User Flipped " + cardsInPlay);
