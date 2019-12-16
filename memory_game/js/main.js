c7a0627bf49883e832955f276d4da6153fd8ada0
Jamal Maudib McDowell <jamalmaudibmcdowell@192.168.1.4>
2019-12-16 12:50:43 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,44 @@
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
 var cardsInPlay = [];
 function checkForMatch(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
     console.log("You Found A Match!");
+    alert("You Found A Match!");
   } else {
     console.log("Sorry,try again")
+    alert("Sorry, try again")
   }
 }
 function flipCard(cardID){
-cardsInPlay.push(cards[cardID])
+cardsInPlay.push(cards[cardID].rank)
 
-console.log("The card you flipped over is " + cards[cardID])
-
+console.log("The card you flipped over is " + cards[cardID].rank)
+console.log(cards[cardID].cardImage)
+console.log(cards[cardID].suit)
 }
 flipCard(0);
 flipCard(2);
